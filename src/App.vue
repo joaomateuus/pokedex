ac7d8566c9c689df00209052a9bdab2772ac096d
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-03-06 23:17:00 -0300

    sass update and pokedex component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="pokedex">
-		<h1>Hello World</h1>
+		<Pokedex />
   </div>
 </template>
 
 <script>
+import Pokedex from './components/Pokedex';
 
 
 export default {
   name: 'App',
   components: {
+		Pokedex,
   },
 };
 </script>
@@ -18,10 +20,10 @@ export default {
 #pokedex {
   display: flex;
 	flex-direction: column;
-	min-height: 100vh;
-	max-width: 1000px;
-	margin: 0 auto;
-	padding: 24px;
+	justify-content: center;
+	align-items: center;
+	height: calc(100vh - 20px);
+	padding: 20px;
 }
 
 </style>
---
+++ src/components/Pokedex.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="pokedex">
+
+	<!---
+	<TopBoard />
+	<LeftBoard class="pokedex-left" />
+	<Divisor />
+	<RightBoard />
+	-->
+  </div>
+</template>
+
+<script>
+/*
+import TopBoard from './TopBoard/TopBoard.vue'
+import LeftBoard from './LeftBoard/LeftBoard.vue'
+import RightBoard from './RightBoard/RightBoard.vue'
+import Divisor from './Divisor/Divisor.vue'
+*/
+export default {
+  name: 'Pokedex',
+ /*
+  components:
+  {
+	TopBoard,
+	LeftBoard,
+	Divisor,
+	RightBoard,
+  }
+  */
+};
+</script>
+
+<style lang="scss">
+.pokedex {
+	position: relative;
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	max-width:960px;
+	height: 100%;
+
+	@media (min-width: $viewport-medium) {
+		max-height: 700px;
+		flex-direction: row;
+	}
+
+	&--left,
+	&--right {
+		position: relative;
+		display: flex;
+		height: 50%;
+		perspective: 1700px;
+		transform-style: preserve-3d;
+
+		@media (min-width: $viewport-medium) {
+			height: 100%;
+		}
+	}
+}
+
+</style>
