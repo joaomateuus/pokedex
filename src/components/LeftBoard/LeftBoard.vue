24830656cdf068582e3b44a9fc209f71b9cd9ac0
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-03-11 20:52:14 -0300

    left board and list file

---
+++ src/components/LeftBoard/LeftBoard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="panel">
+	  <LeftPanelContent />
+  </div>
+</template>
+
+<script>
+import LeftBoardContent from './LeftBoardContent.vue'
+export default {
+	name: 'LeftBoard',
+	components: {
+		LeftBoardContent,
+	}
+
+}
+</script>
+
+<style lang="scss" scoped>
+.panel{
+	display:flex;
+	width: 100%;
+	padding: 85px 20px 20px;
+
+	@media (min-width: $viewport-medium) {
+		width: calc(50% + 20px);
+		padding-right: 60px;
+	}
+}
+
+</style>
+
