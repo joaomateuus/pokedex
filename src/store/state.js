fa3ea1e84ee787412bd47c2a3ed9dda3126a2470
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-03-27 14:02:21 -0300

    setting all states

---
+++ src/store/state.js
@@ -0,0 +1,29 @@
+/*
+here we´ll set our reactive variables and
+vue is going to do all this reactivity for us
+*/
+//import vue to use his observer
+import Vue from 'vue'
+
+const END = 25;
+const BEGIN = 0;
+
+/*Exporting it we can use it everywhere, and as is reactivity
+if somepoint some change happens w the var state it will reflect here*/
+export default Vue.observable({
+	list: [],
+	tempList: [],
+	anyNext: false,
+	isCompleted: false,
+	anyError: false,
+
+	isSearching: false,
+	isPokemonSearching: false,
+	anySearchError: false,
+
+	end: END,
+	begin: BEGIN,
+});
+
+
+
